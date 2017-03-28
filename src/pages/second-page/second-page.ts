a406fc2d7e8dc202df03b70706ec86eabb3cbdaa
mhartington <mikehartington@gmail.com>
2017-03-28 12:54:52 -0400

    init

---
+++ src/pages/second-page/second-page.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { NavController, IonicPage } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'second-page',
+  templateUrl: 'second-page.html'
+})
+export class SecondPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
